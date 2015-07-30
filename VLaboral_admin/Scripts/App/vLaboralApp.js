8632676139cc13beefffad931f4419ee7899034a
kikexp <enriquepaz.inbox@hotmail.com>
2015-07-30 15:20:09 -0300

    Agrego ngAutocomplete y location, cambios en ofertaList y empleadoAdd.

--- VLaboral_admin/Scripts/App/vLaboralApp.js
+++ VLaboral_admin/Scripts/App/vLaboralApp.js
@@ -1,6 +1,6 @@
 ﻿/// <reference path="Oferta/ofertaCtrl.js" />
 var vLaboralApp = angular.module('vLaboralApp', ['ngRoute', 'ngResource', 'ui.router', 'ngCookies', 'ui.bootstrap', 'ngTable',
-  'ngSanitize', 'ngAnimate', 'ui.select', 'ct.ui.router.extras','angular-loading-bar','flow'])
+  'ngSanitize', 'ngAnimate', 'ui.select', 'ct.ui.router.extras','angular-loading-bar','flow', 'ngAutocomplete'])
     .config(function ($stateProvider, $urlRouterProvider, $httpProvider, $stickyStateProvider, cfpLoadingBarProvider) {
 
         cfpLoadingBarProvider.includeSpinner = true;
